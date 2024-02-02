5a7b3eff0c003f853f1235108ed38ef935773050
Lina <lina.puksme@gmail.com>
2024-02-02 10:52:18 +0200

    january 3 class practise

---
+++ 2024-01-02-practise-lesson/coctail-bar-app/script.js
@@ -0,0 +1,115 @@
+//PLANAS:
+// 1. Uzsipildyti filtro selektus duomenimis. DONE
+// 2. Gauname visus gerimus is API - turi buti atliekamas tik tada kai pries tai yra atlikti fetchai gauti kokteiliu kategorijai, siktlines info, ingridientai, todel reikia pasinaudoti asichronnine funjcija kuri pirma kreipsis i pastaruosius API, o tada tik i  gerimo API - DONE
+// 3. Juos atvaizduojame. DONE
+// 4. Atlikti filtracijas kokteiliams
+// 5. Paieska pagal pavadinima
+// 6. Modalinio lango sukurimas.
+// 7. Modaliinio lango uzdarymas.
+// 8. Random kokteilio sugeneravimas is API //www.thecocktaildb.com/api/json/v1/1/random.php
+
+
+// 1.1. isivedam kintamuosius nuskaityti HTML
+const coctailNameFilterElement = document.querySelector("#coctail-name-filter"),
+    categorySelectElement = document.querySelector("#category-select"),
+    glassSelectElement = document.querySelector("#glass-type-select"),
+    ingridientSelectElement = document.querySelector("#ingridient-select"),
+    searchButtonElement = document.querySelector("#search"),
+    iMLuckyButtonElement = document.querySelector("#im-lucky"),
+    dynamicDrinksElement = document.querySelector(".drinks"); //3.3
+
+const categoriesAarray = [], // 1.3.1 kinamasis masyvui
+    drinksArray = []; // 2.9
+
+// API
+async function fillSelectElements() //2.2
+{
+    // 1.2 Kategoriju API
+    await fetch ("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+        .then((response ) => response.json())
+        .then(response => { fillCategorySelect(response.drinks, categorySelectElement, "strCategory")
+        // Map metodas grazina is objetu masyvo objektu laukus strCategory
+        categoriesAarray.push(...response.drinks.map((value)=>value.strCategory)); // 2.6 prie masyvo pridedam masyva su spread operatoriumi 2.7. su map keiciame masyvo reiksmes i string
+        }) //1.3.2 gauname viena objekta, kuris sudarytas is masyvo,, kuriame yra objektas, kur objektas turi tik viena lauka strCategory
+        .catch((error) => console.log(error))
+        // .finally((response) => console.log("Uzklausa baigta")); // kai pazadas yra ivykdytas t.y.suveikia abu then arba catch, tik tada paleidziamas finally, realiai cia nelb reikalingas
+        // reiks prasukti cikla pro visus situos obbjektus ir steCategory prisideti prie categorySelectElement laukelio
+
+    // 1.4 Stikliniu tipu API
+    await fetch ("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
+    .then((response ) => response.json())
+    .then(response => fillCategorySelect(response.drinks, glassSelectElement, "strGlass")) //console.log(response))  patikrinam objekto sandara, koks lauko pavadinimas, siuo atveju yra strGlass:
+    .catch((error) => console.log(error))
+
+    // 1.6 Ingridientu tipu API
+    await fetch ("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+    .then((response ) => response.json())
+    .then(response => fillCategorySelect(response.drinks, ingridientSelectElement, "strIngredient1")) //strIngredient1:
+    .catch((error) => console.log(error))
+}
+
+// 1.3 Kategorijos select'o uzpildymas
+function fillCategorySelect (properties, selectElement, strFieldName) // 1.5 papildyta taip kad tiktu ir kitu fieldu fetchams
+{   
+    let dynamicHTML = ''; //1.3.4 selecto option'ai generuojami automatiskai, todel dinaminiam HTMLui pridedamas kintamasis
+    for(const property of properties)
+    {
+        dynamicHTML += `<option>${property[strFieldName]}</option>` //1.3.5 optionu pridejimas
+        // console.log(category.strCategory) //1.3.1
+        // categoriesAarray.push(property.strCategory) // kategoriju masyvo pridejimas, uzkomenuotas nes prkelta i kita funkcija
+    }
+    selectElement.innerHTML = dynamicHTML; //1.3.6 atvaizdavimo HTMLe pridejimas
+    // console.log(categoriesAarray); //1.3.3 matome jog gavome kategoriju selekto duomenis
+}
+
+
+async function getAllDrinks() //2.8 kadangi gavome 11 kokteiliu kategoriju, todel bus 11 fetchu gauti visus kokteilius
+{
+// https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink URL skirtas gauti gerimus pagal kategorija
+    for(const category of categoriesAarray) // 2.10
+    {
+        //2.11 reikia susigeneruoti dinamini URL tam kad gauti gerima pper kategorija, gerimo kategorija sudaryta is daugiau nei vieno zodzio atskyriama "_"
+        // vietoj tarpu iterpimas buksniuku galimas keliais budais:
+        // I - pasplitinti pavadinima per tarpus, pasiversti i masyva,, o paskui join su apatiniu bruksniuku
+        // II - pasinaudoti replaceAll metodu (sitas metoas pasirinktas)
+        let dynamicURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category.replaceAll(" ","_")}`;
+        // console.log(dynamicURL) // patikrinti ar gerai sugeneruojamas URL (reik butinai, jog butu iskvietimas funkcios kitaip nesiamto consol loge)
+        // 2.12
+        const response = await fetch(dynamicURL);
+        const answerFromServer = await response.json(); // tokiu budu nereikia naudoti then metodo ir galima pasiimti taip atsakyma ir response'o t.y. tai aletrnatyva. then metodui
+        for(const drink of answerFromServer.drinks) //2.14
+        {
+            drinksArray.push(drink);
+        }
+        
+        // console.log(answerFromServer) // patikrinam koks atsakymas guanamas
+        // drinksArray.push(answerFromServer);
+
+    }
+}
+
+function generateDrinksHTML(drinks) // 3.1 // pridedam parametra, nes norima bus atvaizduoti gerimus ne visada visu is masyvo drinksArray, bet ir atfiltruotus
+{
+    let dynamicHTML = '';
+    for (let drink of drinks)
+    {
+        dynamicHTML += `<div class="drink">
+        <img src="${drink.strDrinkThumb}" alt="">
+        <h2 class="drink-title">${drink.strDrink}</h2>
+    </div>`;
+    }
+    dynamicDrinksElement.innerHTML = dynamicHTML; //3.4
+}
+// 2.
+// 2.1.
+async function initialization() // initialization yra toks laiko tarpas per kuri pirma surenkami tokie duomenys kurie uzpildomo filtro elementus, tai cia toks pirmas etapas, o antras etapas buna gerimu atvaizdavimas ir tada inicializacijos pabaiga, ir aplikacija jau pati nieko nebeatliks ir lauks vartotojjo veiksmu.
+{
+    // I etapas. Select'u uzpildymas.
+    await fillSelectElements(); // 2.4 await - uzdeda parametra, jog turi palaukti, kol bus atlikta
+    // console.log(categoriesAarray) //2.6 perziurim ar gerai gauname masyva 2.11 perziureti kokie gaunami pavdinimai, kad zinoti kaip URL suformuoti tarpus jei daugiau nei veinas zodis
+    // II etapas. Dinaminis gerimu atvaizdavimas.
+    await getAllDrinks() // 2.13 pridetas wait, todel gaunami visi gerimai ir tik tada atvaizduojamas consolelogas
+    generateDrinksHTML(drinksArray); //3.5
+    // console.log(drinksArray) // 2.12 patikrinti ar visi gerimai sudedami i masyva, matome jog gauname masyva objektu pavidalu masyvus, todel reik sumapinti, kad butu tiesiogiai objektai 
+}
+initialization() // 2.5 funkkcijos iskvietimas
---
+++ 2024-01-03-practise-lesson/await-promise-all.js
@@ -0,0 +1,140 @@
+/* const allUrls1 = [
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka",
+	],
+	allUrls2 = [
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Scotch",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Triple_sec",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Applejack",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka",
+	],
+	allUrls3 = [
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Scotch",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Triple_sec",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Applejack",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Brandy",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Lemon_vodka",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Champagne",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Sherry",
+	];
+
+// PAPRASTAS FETCH PAPRASTOJE FUNKCIJOJE
+// noras yra ismatuoti, kiek trunka uzklausa i API, todel i cosole irasome "new Date()" arba date.getTime - gauname kiek praejo sekundziu nuuo 1970- sausio 1d. skaicius praejes nuoit os datos vadinasi time stamp
+function fetchData()
+{
+    const startTime = new Date();
+    fetch(allUrls2[0])
+    // GET (su sio tipo request'u dabar mokomes dirbti), POST, UPDATE(updatinti visa objekta), PATCH(updatinti tik tam tikrus laukus), DELETE (ateityje mokinsimes)
+    .then((response) => response.json())
+    .then((response) => {
+        // console.log(response);
+        const endTime = new Date(); 
+        const duration = endTime.getTime() - startTime.getTime(); //apskaiciuojamas timeStamp nou 1970, kai buvo sukurta UTC 32 bitu sistema
+        console.log(duration +"ms"+" function");
+        }); //kadangi bus jau ne be viena israiska todel dedame {}
+}
+fetchData();
+
+// SINCHRONINE FUNKCIJA
+// irgi turimas tiksas ismatuoti, kiek trunka uzklausa i API
+async function fetchDataAsync()
+    {
+        const startTime = new Date();
+        const request = await fetch(allUrls2[0]);
+        const drinks = await request.json();
+        const endTime = new Date();
+        const duration = endTime.getTime() - startTime.getTime();
+        console.log(duration +"ms"+" async");
+    }
+fetchDataAsync()
+
+// Kreipimasis i visus URL vienu metu.
+// Galima atlikti dviem budais,abi funkcijos asinchronines:
+// I Budas - pasinaudojant ciklu (fetchins viena URL, tada kita ir taip per visa masyva) todel sitas budas netaupus laiko atzvilgiu
+async function fetchAllDataByIngredient(urls)
+    {
+        const startTime = new Date();
+        const array = [];
+        for(const url of urls)
+        {
+            const promise =  await fetch(url);
+            const answer = await promise.json();
+            array.push(answer);
+            // console.log(answer);
+        }
+        const endTime = new Date();
+        const duration = endTime.getTime() - startTime.getTime();
+        console.log(duration +"ms"+" async I budas - one by one");
+    }
+fetchAllDataByIngredient(allUrls3)//paeiliui sukrenta fetchas ir jo response rezultatai
+
+// II Budas - kaip issiusti requesta visiems vienu metu
+async function fetchAllDataIngredient(urls)
+{
+    const startTime = new Date();
+    const promises = urls.map((url) => 
+        fetch (url).then(response=>response.json())); //masyvas sudarytas is pazadu, kurie veliau Promise.all(promises) yra islaukiami
+    // console.log(promises);
+    const dataFromAllSources = await Promise.all(promises) //automatinis visu uzkalusu palaukimas atliekamas taip
+    // console.log(dataFromAllSources);
+    const endTime = new Date();
+    const duration = endTime.getTime() - startTime.getTime();
+    console.log(duration +"ms"+" async II budas - all at the same time");
+}
+fetchAllDataIngredient(allUrls3) */
+
+const array = [1, 2, 3, 4];
+//masyvo kopijos sukurimas
+const array2 = array; //cia paduodamas reference 
+console.log(array2);
+//norint kad nepasikeistu reiktu naudoti spredinima:
+const array3 = [...array] // taip sukuriam masyvo kopija, o  cia paduodamas visas masyvas
+console.log(array3);
+array.push(5);
+console.log(array);
+console.log(array2); // pasikeite masyvas ir array2
+
+// MASYVO DESTRUKTURAVIMAS
+// UZDUOTIS is masyvo array reik pirmuju 3 reiksmiu:
+// const [pirmas, antras, trecias] = array;
+// console.log(pirmas);
+// console.log(antras);
+// console.log(trecias);
+
+//jei reikia pirmo ir trecio tada taip atrodo masyvas:
+// const [pirmas, , trecias] = array;
+
+const [, antras, ,ketvirtas] = array;
+console.log(antras);
+console.log(ketvirtas);
+
+// OBJEKTO DESTRUKTURAVIMAS
+// UZDUOTIS istraukti is objekto reiksmes:
+const obj = {
+    id: 9,
+    name: "John",
+    surname: "Doe",
+    age: 27,
+    married: false,
+}
+console.log(obj.id);
+console.log(obj["id"]);
+
+const {name, surname, married, id} = obj
+console.log(name, surname, married);
+
+// MAP metodas:
+const arrayForMap = [1, 2, 3, 4, 5];
+const mappedArray = arrayForMap.map((value) => value*2);
+console.log(mappedArray);
+console.log(arrayForMap); // lieka nepakites
+
+// FILTER metodas:
+const arrayForFilter = [1, 2, 3, 4, 5];
+const filteredArray = array.filter((value)=> value <= 3);
+console.log(filteredArray);
+
+// SOME metodas (sis metodas klausia, ar masyve egzistuouja kazkokia reiksme, jei nors viena reiksme masyve atitinka salyga grazina sis metodas true). Sitas metodas dazna naudojamas su filtru
+const isThereAWantedValue = array.some((value) => value === 3); // daznai sitas metodas naudojamas objektuose tokiu budu pvz ieskant id array.some((value) => value.id === 3)
+console.log(isThereAWantedValue);
